0591d415417ece88d56df8ad9c9a667407fe9b0c
sepehrjavid <javid.sepehr77@gmail.com>
2019-11-11 13:44:38 +0330

    allow blank in note body and set datTime to update on edit

--- note/models.py
+++ note/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Note(models.Model):
     category = models.ForeignKey("category.Category", on_delete=models.CASCADE, related_name="notes")
     title = models.CharField(max_length=20)
-    body = models.TextField()
+    body = models.TextField(blank=True)
     isImportant = models.BooleanField(default=False)
     isPerformed = models.BooleanField(default=False)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
